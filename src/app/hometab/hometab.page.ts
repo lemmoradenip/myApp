11926531b05c77edf3d3368e32f22358e3129843
rommel-desktop <rmmelpneda.myphone@gmail.com>
2019-06-17 18:08:18 +0300

    progress bar based on total seconds mode

--- src/app/hometab/hometab.page.ts
+++ src/app/hometab/hometab.page.ts
@@ -7,20 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './hometab.page.html',
   styleUrls: ['./hometab.page.scss'],
 })
-export class HometabPage  {
+export class HometabPage {
   percent: number;
   radius: number = 100;
-  fullTime: any = '00:01:30';
+  fullTime: any = '00:00:30';
 
   timer: any = false;
   progress: any = 0;
-  minutes: number = 1;
+  minutes: any = 1;
   seconds: any = 30;
 
 
 
   startTime() {
-    clearInterval(this.timer);
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
     this.timer = false;
     this.percent = 0;
     this.progress = 0;
@@ -29,19 +31,28 @@ export class HometabPage  {
     this.minutes = timeSplit[1];
     this.seconds = timeSplit[2];
 
-    let totalSeconds = Math.floor(this.minutes * 60) * parseInt(this.seconds);
+    let totalSeconds = Math.round(parseInt(this.minutes) * 60) + parseInt(this.seconds);
+
+    console.log(totalSeconds);
+    // this.timer = setInterval(() => {
+
+    //   if (this.percent == this.radius) {
+    //     clearInterval(this.timer);
+    //   }
+    //   this.percent = totalSeconds;//Math.round(((this.progress / totalSeconds) * 100) * 100);//Math.floor((this.progress / totalSeconds) * 100); // this will return     
+    //   console.log(totalSeconds);
+    //   this.progress++;
+    // }, 1000)
 
     this.timer = setInterval(() => {
-
-      if (this.percent == this.radius){
-        clearInterval(this.timer);}
-
-      this.percent = Math.round(((this.progress / totalSeconds) * 100)*100);//Math.floor((this.progress / totalSeconds) * 100); // this will return
-      this.progress++;    
+      if (this.progress == totalSeconds) {
+        clearInterval(this.timer);
+      }
+      this.percent = this.progress; // this equivalent to total seconds
       console.log(this.percent);
-    
+      this.progress++
     }, 1000)
-    
+
   }
   // constructor(private menu: MenuController) { }
   // openFirst() {
