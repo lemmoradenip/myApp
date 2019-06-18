3bd030aa22ae658d002560b334258efb218e7b9f
rommel-desktop <rmmelpneda.myphone@gmail.com>
2019-06-18 14:40:42 +0300

    just added lapsed time and fab icon

--- src/app/hometab/hometab.page.ts
+++ src/app/hometab/hometab.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { MenuController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
 
 
 @Component({
@@ -10,19 +10,32 @@ import { Component } from '@angular/core';
 export class HometabPage {
   percent: number;
   radius: number = 100;
-  fullTime: any = '00:00:30';
+  fullTime: any = '00:00:10';
 
   timer: any = false;
   progress: any = 0;
-  minutes: any = 1;
+  minutes: any = 0;
   seconds: any = 30;
 
+  elapsed: any = {
+    h: '00',
+    m: '00',
+    s: '00'
+  }
+  overalltimer: any = false;
 
 
   startTime() {
     if (this.timer) {
       clearInterval(this.timer);
     }
+    if (!this.overalltimer) {
+      this.progressTimer();
+    }
+    else {
+      clearInterval(this.overalltimer);
+    }
+
     this.timer = false;
     this.percent = 0;
     this.progress = 0;
@@ -31,38 +44,81 @@ export class HometabPage {
     this.minutes = timeSplit[1];
     this.seconds = timeSplit[2];
 
+
+
     let totalSeconds = Math.round(parseInt(this.minutes) * 60) + parseInt(this.seconds);
-
-    console.log(totalSeconds);
-    // this.timer = setInterval(() => {
-
-    //   if (this.percent == this.radius) {
-    //     clearInterval(this.timer);
-    //   }
-    //   this.percent = totalSeconds;//Math.round(((this.progress / totalSeconds) * 100) * 100);//Math.floor((this.progress / totalSeconds) * 100); // this will return     
-    //   console.log(totalSeconds);
-    //   this.progress++;
-    // }, 1000)
 
     this.timer = setInterval(() => {
       if (this.progress == totalSeconds) {
         clearInterval(this.timer);
       }
-      this.percent = this.progress; // this equivalent to total seconds
-      console.log(this.percent);
+      this.percent = Math.round((this.progress / totalSeconds) * 100); // this equivalent to total seconds
+
+      /* if (!this.overalltimer) {
+         this.elapsed.h = this.pad(0, 2);
+         this.elapsed.m = this.pad(this.elapsed.m, 2);
+         this.elapsed.s = this.pad(this.elapsed.s, 2);
+       }*/
+
       this.progress++
     }, 1000)
 
   }
-  // constructor(private menu: MenuController) { }
-  // openFirst() {
-  //   this.menu.enable(true, 'first');
-  //   this.menu.open('first');
-  // }
-  // openSecond() {
-  //   this.menu.enable(true, 'second');
-  //   this.menu.open('second');
-  // }
+
+  progressTimer() {
+    let countDownDate = new Date();
+    this.overalltimer = setInterval(() => {
+      if (!this.overalltimer) {
+        this.progressTimer();
+      }
+
+
+      let now = new Date().getTime();
+      let distance = now - countDownDate.getTime();
+      console.log(distance);
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60) / (1000 * 60)));
+      this.elapsed.s = Math.floor((distance % (1000 * 60) / 1000));
+
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+    }, 1000);
+  }
+  pad(num, size) {
+    let s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+
+
+  stopTime() {
+    clearInterval(this.timer);
+    clearInterval(this.overalltimer);
+    this.overalltimer = false;
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+    this.elapsed = {
+      h: '00',
+      m: '00',
+      s: '00'
+    }
+  }
+
+
+
+
+  constructor(private menu: MenuController) { }
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+  openSecond() {
+    this.menu.enable(true, 'second');
+    this.menu.open('second');
+  }
   // ngOnInit() {
 
   // }
